c30bf35fdf804c3c573e95945c54b3d3de834140
madhu2910 <78549424+madhu2910@users.noreply.github.com>
2021-04-24 14:13:06 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,86 +1,56 @@
-import React from "react";
-import { StyleSheet, Text, View ,TouchableOpacity, Image, StatusBar,ScrollView, Button, Alert, FlatList} from "react-native";
-import demodata from '.demodata/';
-class App extends React.Component {
-  constructor(props)
-  {
-    super(props);
-    this.state={
-      isLoading:false,
-      demodata:[
-        {name: 'Michael'},
-    {name: 'Lindsay'},
-    {name: 'Tobias'},
-    {name: 'Byron'},
-    {name:'George'},
-    {name: 'Rachel'}
-      ]
-    };
-  }
-  componentDidMount(){
-   let arr= this.state.demodata.map((item, index)=>{
-     item.isSelect = false
-     return {...item};
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-   })
-   this.setState({demodata : arr});
-   console.log('arr data ==>', arr);
-  }
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
 
-  goToLoad = () => {
-    this.setState({isLoading: true});
+const Stack = createStackNavigator();
 
-  };
-  selectionHandler=()=> {
-    isSelect : false
-
-  }
-  
-render(){
-  const {isLoading, demodata} = this.state;
-
-  
-        
+function MyStack() {
   return (
-    <View style= {{flex: 1}}>
-      <StatusBar barStyle="dark-content"/>
-      
-        <View
-        style={{
-          flex:1,
-          justifyContent:'center',
-          alignItems: 'center',
-        
-        
-        }}/>
-        
-          {demodata.map((item, index)=> {
-            return(
-              <TouchableOpacity
-              onPress={()=> this.selectionHandler()}
-              style={{
-                marginTop: 20,
-                height: 50,
-                width: '80%',
-                borderRadius: 12,
-                backgroundColor: 'green',
-                justifyContent:'space-between',
-                flexDirection:'row',
-                paddingHorizontal: 25,
-                alignItems: 'center',
-              }}>
-                <Text style={{color: 'white', fontSize: 18}}>
-                  {item.name}
-                  </Text>
-                  <Text style={{color: 'white', fontSize: 18}}>
-                    
-                </Text>
-              </TouchableOpacity>
-          
-          
-            
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen 
+        name="Signup" 
+        component={Signup} 
+        options={{ title: 'Signup' }}
+      />       
+      <Stack.Screen 
+        name="Login" 
+        component={Login} 
+        options={
+          {title: 'Login'},
+          {headerLeft: null} 
+        }
+      />
+      <Stack.Screen 
+       name="Dashboard" 
+       component={Dashboard} 
+       options={
+         { title: 'Dashboard' },
+         {headerLeft: null} 
+       }
+      />
+    </Stack.Navigator>
+  );
+}
 
-              
-            );
-            })}</View>
-
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
