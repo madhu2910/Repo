f4510bdf28a5277ab963b48f7bd8e6682e8d7ac1
madhu2910 <78549424+madhu2910@users.noreply.github.com>
2021-07-29 11:54:27 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,59 +1,76 @@
-import  React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 
-import Login from './Component/Login';
-import Signup from './Component/Signup';
-import Verification from './Component/Verification';
-
+import SplashScreen from './Screen/SplashScreen';
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import DrawerNavigatorRoutes from './Screen/DrawerNavigatorRoutes';
 
 const Stack = createStackNavigator();
 
-function MyStack() {
+const Auth = () => {
+  // Stack Navigator for Login and Sign up Screen
   return (
-    <Stack.Navigator
-      initialRouteName="Signup"
-      screenOptions={{
-        headerTitleAlign: 'center',
-        headerStyle: {
-          backgroundColor: '#3740FE',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen 
-        name="Signup" 
-        component={Signup} 
-        options={{ title: 'Signup' }}
-      />       
-      <Stack.Screen 
-        name="Login" 
-        component={Login} 
-        options={
-          {title: 'Login'},
-          {headerLeft: null} 
-        }
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{headerShown: false}}
       />
-      <Stack.Screen 
-       name="Verification" 
-       component={Verification} 
-       options={
-         { title: 'Verification' },
-         {headerLeft: null} 
-       }
+      <Stack.Screen
+        name="RegisterScreen"
+        component={RegisterScreen}
+        options={{
+          title: 'Register', //Set Header Title
+          headerStyle: {
+            backgroundColor: '#307ecc', //Set Header color
+          },
+          headerTintColor: '#fff', //Set Header text color
+          headerTitleStyle: {
+            fontWeight: 'bold', //Set Header text style
+          },
+        }}
       />
-      
+      <Stack.Screen
+        name="DrawerNavigatorRoutes"
+        component={DrawerNavigatorRoutes}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
-}
+};
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <MyStack />
+      <Stack.Navigator initialRouteName="SplashScreen">
+        {/* SplashScreen which will come once for 5 Seconds */}
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          // Hiding header for Splash Screen
+          options={{headerShown: false}}
+        />
+        {/* Auth Navigator: Include Login and Signup */}
+        <Stack.Screen
+          name="Auth"
+          component={Auth}
+          options={{headerShown: false}}
+        />
+        {/* Navigation Drawer as a landing page */}
+        <Stack.Screen
+          name="DrawerNavigatorRoutes"
+          component={DrawerNavigatornRoutes}
+          // Hiding header for Navigation Drawer
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
